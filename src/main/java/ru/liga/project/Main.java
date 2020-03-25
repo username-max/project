5549568b3a3b6fc050c3551b20f49c5c87b1a0e2
vkoba <vkoba@phoenixit.ru>
2020-03-25 19:03:29 +0300

    for start

--- src/main/java/ru/liga/project/Main.java
+++ src/main/java/ru/liga/project/Main.java
@@ -1,25 +1,20 @@
 package ru.liga.project;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.liga.project.domain.InfectionModelling;
 import ru.liga.project.domain.InfectionStatistic;
+import ru.liga.project.repository.DbVirusRepository;
+import ru.liga.project.repository.MemoryPersonRepository;
 
 public class Main {
 
     public static void main(String[] args) {
-//        InfectionStatistic infectionStatistic = new InfectionStatistic(
-//                new InfectionModelling(
-//                        new DbRepository("jdbcurl", "login", "pass"),
-//                        new MemoryRepository()
-//                )
-//        );
+        InfectionStatistic infectionStatistic = new InfectionStatistic(
+                new InfectionModelling(
+                        new DbVirusRepository("jdbcurl", "login", "pass"),
+                        new MemoryPersonRepository()
+                )
+        );
 
-//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext("ru.*");
-        InfectionStatistic istat = (InfectionStatistic) context.getBean("infectionStatistic");
-        istat.tryNTimesAndPrint(5);
-//        infectionStatistic.tryNTimesAndPrint(5);
+        infectionStatistic.tryNTimesAndPrint(5);
     }
 }
